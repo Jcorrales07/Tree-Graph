b820de9bba05fab5bca0873ba75a94376753e51e
Jcorrales07 <corralesjoe07@gmail.com>
2022-09-22 20:37:32 -0600

    Esqueleto de TDAArbol (Tengo un dilema)

--- TDAArbol.cpp
+++ TDAArbol.cpp
@@ -1,5 +1,40 @@
-//
-// Created by Corra on 9/22/2022.
-//
-
 #include "TDAArbol.h"
+
+
+//Constructor
+TDAArbol::TDAArbol() {
+    raiz = nullptr;
+}
+
+//Destructor
+TDAArbol::~TDAArbol() {
+    TDAArbol::vaciar();
+}
+
+
+//Inserta un nodo en el arbol
+void TDAArbol::insertar(Nodo *nodo) {
+    if (estaVacio()) {
+        raiz = nodo;
+        return;
+    }
+
+    // Hacer logica que inserte el nodo en el arbol
+}
+
+//Elimina un nodo del arbol
+void TDAArbol::eliminar(Nodo *nodo) {
+    if (estaVacio()) {
+        return;
+    }
+
+    // Hacer logica que elimine el nodo del arbol
+}
+
+
+//Funcion que verifica si el arbol esta vacio
+bool TDAArbol::estaVacio() {
+    return raiz == nullptr;
+}
+
+
--- TDAArbol.h
+++ TDAArbol.h
@@ -1,12 +1,35 @@
 #ifndef TDAARBOL_H
 #define TDAARBOL_H
 
+#include "Object.h"
+#include "Nodo.h"
 
-class TDAArbol {
+class TDAArbol : public Object {
 
+    public:
 
+        TDAArbol();
+        ~TDAArbol();
 
+        virtual void            insertar(Nodo*);
+        virtual void            eliminar(Nodo*);
+        virtual bool            buscar(Nodo*);
+        virtual void            imprimir();
+        virtual void            imprimirInOrder();
+        virtual void            imprimirPreOrder();
+        virtual void            imprimirPostOrder();
+        virtual void            vaciar();
+        virtual bool            estaVacio();
+        virtual int             getAltura();
+        virtual int             getNivel();
+        virtual int             getTamanio();
+        virtual Nodo*           getRaiz();
+        virtual void            setRaiz(Nodo*);
+        bool                    equals(Nodo*);
+        string                  toString();
 
+    private:
+        Nodo*                   raiz;
 
 };
 
